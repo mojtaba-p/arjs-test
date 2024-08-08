10d33a56f85a056ac10331ea2694bf3a1572a557
Mojtaba Pirhorilar <56000277+mojtaba-p@users.noreply.github.com>
2024-08-08 13:08:25 +0330

    fix: mark file name

--- index.php
+++ index.php
@@ -59,7 +59,7 @@
         <a-nft
         id="handler"
             videohandler
-            type='nft' url='./mavaranet'
+            type='nft' url='./mavarant'
             smooth="true" smoothCount="10" smoothTolerance="0.01" smoothThreshold="5"
         >
             <a-video
