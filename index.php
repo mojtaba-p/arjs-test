183b12ff395ea1d7e6d0fe3e72f1eb467c9bee8c
Mojtaba Pirhorilar <56000277+mojtaba-p@users.noreply.github.com>
2024-08-08 12:54:09 +0330

    absolute url for marker

--- index.php
+++ index.php
@@ -59,7 +59,7 @@
         <a-nft
         id="handler"
             videohandler
-            type='nft' url='./filimoo'
+            type='nft' url='/arjs-test/filimoo'
             smooth="true" smoothCount="10" smoothTolerance="0.01" smoothThreshold="5"
         >
             <a-video
