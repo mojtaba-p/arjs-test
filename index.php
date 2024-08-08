6a87843646ed6b4a03c5a4cb0e7cde4b1b398f68
Mojtaba Pirhorilar <56000277+mojtaba-p@users.noreply.github.com>
2024-08-08 13:04:35 +0330

    wip: scan mavaranet logo

--- index.php
+++ index.php
@@ -59,7 +59,7 @@
         <a-nft
         id="handler"
             videohandler
-            type='nft' url='/arjs-test/filimoo'
+            type='nft' url='./mavaranet'
             smooth="true" smoothCount="10" smoothTolerance="0.01" smoothThreshold="5"
         >
             <a-video
